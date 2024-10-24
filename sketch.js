1e0b65218d98ee3a6ecc84c0a6b24706fa65b087
Reginaldo Guntowski Gouvea <161737727+R3G1NH0@users.noreply.github.com>
2024-10-24 11:25:45 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,67 @@
 // Infantil, Comédia, Animação, Fantasia, Terror, Mistério, Suspense, Aventura, Família, Ficção Científica, Ação, Faroeste
 
-// Divertida Mente, Livre, Infantil, Comédia
-// Hotel Transilvânia 2, Livre, Animação, Comédia, Fantasia
+// Divertida Mente, 12, Infantil, Comédia
+// Hotel Transilvânia 2, 10, Animação, Comédia, Fantasia
 
-// O Exorcista, 14, Terror, Mistério
-// A Entidade, 16, Terror, Suspense
+// Os Goonies, 10, aventura
+// Uncharted: Fora do Mapa, 12, aventura, Ação
 // A Freira 2, 16, Terror, Mistério
 // Assim na Terra Como no Inferno, 14, Terror, Aventura
-// Wall-E, Livre,  Aventura, Animação, Família, Ficção Científica
+// Wall-E, 10,  Aventura, Animação, Família, Ficção Científica
 // Star Wars, 14, Ficção científica, Ação
 // Círculo de Fogo, 12, Ação, Aventura
 // ridiculous 6, 14,  Comédia, Faroeste
 
+let campoIdade;
+let campoFantasia;
+let campoAventura;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("10");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
 }
 
 function draw() {
-  background(220);
-  let recomendacao = geraRecomendacao();
+  background("rgb(25,218,218)");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
   text(recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao() {
-  if(idade >= 10) {
-    
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "ridiculous 6";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Círculo de Fogo";          
+        } else{
+         return "Divertida Mente";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "Wall-E";
+        } else {
+          return "Hotel Transilvânia 2";
+        }
+      }
+    }
   } else {
-    return "Divertida Mente";
+    if (gostaDeAventura) {
+      return "Uncharted: Fora do Mapa";
+    } else {
+      return "Os Goonies";
+    }
   }
 }
-
-
-
-
-
-
